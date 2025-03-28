71157305dd5bbb73bd566c14437db3112a945824
mmanna3 <mmmannna3@gmail.com>
2025-03-27 23:26:41 -0300

    cambia nombre de repo de app

--- scripts/generar-contrato-be-en-app.sh
+++ scripts/generar-contrato-be-en-app.sh
@@ -5,6 +5,6 @@ cd ../liga-be/Api
 dotnet build
 cd $SCRIPT_DIR
 swagger tofile --output swagger.json ../liga-be/Api/bin/Debug/net8.0/Api.dll v1
-nswag openapi2tsclient /input:swagger.json /output:./../liga-app-delegados/app/api/clients.ts
+nswag openapi2tsclient /input:swagger.json /output:./../app-carnet-digital/app/api/clients.ts
 rm swagger.json
 cd $ME_LLAMARON_DESDE
